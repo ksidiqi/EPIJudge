3f84ee632caf808d466c5254650f85e267487e21
ksidiqi <ksidiqi@users.noreply.github.com>
2019-07-12 01:38:18 -0700

    Update minimum_distance_3_sorted_arrays.py

--- epi_judge_python_solutions/minimum_distance_3_sorted_arrays.py
+++ epi_judge_python_solutions/minimum_distance_3_sorted_arrays.py
@@ -1,30 +1,25 @@
-import bintrees
-
 from test_framework import generic_test
+import sortedcontainers as sc
 
 
 def find_closest_elements_in_sorted_arrays(sorted_arrays):
-
-    min_distance_so_far = float('inf')
-    # Stores array iterators in each entry.
-    iters = bintrees.RBTree()
+    container = sc.SortedDict()
     for idx, sorted_array in enumerate(sorted_arrays):
         it = iter(sorted_array)
-        first_min = next(it, None)
-        if first_min is not None:
-            iters.insert((first_min, idx), it)
-
+        key = next(it, None)
+        if key is not None:
+            container[(key, idx)] = it
+    min_distance = container.iloc[-1][0] - container.iloc[0][0]
+    
     while True:
-        min_value, min_idx = iters.min_key()
-        max_value = iters.max_key()[0]
-        min_distance_so_far = min(max_value - min_value, min_distance_so_far)
-        it = iters.pop_min()[1]
-        next_min = next(it, None)
-        # Return if some array has no remaining elements.
-        if next_min is None:
-            return min_distance_so_far
-        iters.insert((next_min, min_idx), it)
-
+        key_idx , it = container.popitem(index=0)
+        key = next(it, None)
+        if key is not None:
+            container[(key, key_idx[1])] = it
+            min_distance = min(min_distance, container.iloc[-1][0] - container.iloc[0][0])
+        else:
+            break
+    return min_distance
 
 if __name__ == '__main__':
     exit(
